691255f3e68dc47051df0f115fb14b02a9a73717
Alejandro Lopez Rocha <alejandro.rocha@mobgen.com>
2018-03-06 11:05:40 +0100

    Initial Commit

---
+++ iOSTalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSTalk
+//
+//  Created by Alejandro López Rocha on 06/03/2018.
+//  Copyright © 2018 Alejandro López Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
